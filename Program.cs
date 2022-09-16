37604bbaa82fe6ce13d401157b60ff2d5b2ace52
Supanut Chantarasiri <supanut.chan@mail.kmutt.ac.th>
2022-09-16 11:16:26 +0700

    Quiz1

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class Program {
+
+ public static void Main(string[] args) {
+    DogDoor door = new DogDoor();
+    door.Open();
+    door.Close();
+    Console.WriteLine("Door is open ? : {0}", door.IsOpen());
+ }
+
+}
+
+
